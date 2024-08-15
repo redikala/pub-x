ed07bff106bee8a750acc345bbee95c4a3f4b8c8
redikala <eje25sendervip@gmail.com>
2024-08-15 18:05:51 +0700

    Create EJE.js

---
+++ EJE.js
@@ -0,0 +1,17 @@
+addEventListener('fetch', event => {
+  event.respondWith(handleRequest(event.request))
+})
+
+async function handleRequest(request) {
+  // Mengambil respons dari server utama
+  let response = await fetch('https://royale.midasbuyplus.com')
+
+  // Memodifikasi respons jika diperlukan
+  let modifiedResponse = new Response(response.body, response)
+
+  // Menambahkan header atau melakukan manipulasi lainnya
+  modifiedResponse.headers.set('Content-Type', 'text/html')
+
+  // Mengirim respons yang sudah dimodifikasi
+  return modifiedResponse
+}
